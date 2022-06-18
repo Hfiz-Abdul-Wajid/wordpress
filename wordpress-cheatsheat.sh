7f6b31f202f192a1cfc1b1cc867d209d3fdebfb8
Hfiz-Abdul-Wajid <107339686+Hfiz-Abdul-Wajid@users.noreply.github.com>
2022-06-18 08:54:53 +0500

    update date
    update date

---
+++ wordpress-cheatsheat.sh
@@ -0,0 +1,4 @@
+#---- Head to application's public_html/wp-includes
+Run the command: stat -c %y version.php 
+to list the modification date 
+The application modification dates will be the same as the version.php's modification date
